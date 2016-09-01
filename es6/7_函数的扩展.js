97aed9550d922d5bcac30327c64920f91fdb9849
Jon_shen <jerosshen@gmail.com>
2016-09-01 17:48:29 +0800

    回家继续

--- es6/7_函数的扩展.js
+++ es6/7_函数的扩展.js
@@ -100,3 +100,94 @@ foo(undefined, null)
 // 5 null
 
 // 函数的length属性
+// 指定了默认值以后，函数的length属性，将返回没有指定默认值的参数个数。也就是说，指定了默认值后，length属性将失真。
+(function (a) {}).length // 1
+(function (a = 5) {}).length // 0
+(function (a, b, c = 5) {}).length // 2
+// 同理，rest参数也不会计入length属性。
+(function(...args) {}).length // 0
+// 如果设置了默认值的参数不是尾参数，那么length属性也不再计入后面的参数了。
+(function (a = 0, b, c) {}).length // 0
+(function (a, b = 1, c) {}).length // 1
+
+
+// 作用域
+var x = 1;
+function f(x, y = x) {
+  console.log(y);
+}
+f(2) // 2
+// 如果调用时，函数作用域内部的变量x没有生成，结果就会不一样。
+let x = 1;
+function f(y = x) {   // x，还未申明，所以取全局x
+  let x = 2;          // 作用域在括号内
+  console.log(y);
+}
+f() // 1
+// 下面会报错
+function f(y = x) {
+  let x = 2;
+  console.log(y);
+}
+f() // ReferenceError: x is not defined
+下面也会报错
+var x = 1;
+function foo(x = x) {     // 前一个 x 是参数，在默认值前执行了，形成暂时性死区。
+  // ...
+}
+foo() // ReferenceError: x is not defined
+// 如果参数的默认值是一个函数，该函数的作用域是其声明时所在的作用域。请看下面的例子。
+let foo = 'outer';
+function bar(func = x => foo) {
+  let foo = 'inner';
+  console.log(func()); // outer
+}
+bar();
+// 也报错
+function bar(func = () => foo) {
+  let foo = 'inner';
+  console.log(func());
+}
+bar() // ReferenceError: foo is not defined
+
+// 下面是一个更复杂的例子。
+var x = 1;
+function foo(x, y = function() { x = 2; }) {
+  var x = 3;      // 这个x 和 参数内的x没关系
+  y();
+  console.log(x);
+}
+foo() // 3
+去掉重新申明就一样
+var x = 1;
+function foo(x, y = function() { x = 2; }) {
+  x = 3;        // 去掉重新申明 var
+  y();
+  console.log(x);
+}
+foo() // 2
+
+
+// 应用
+// 利用参数默认值，可以指定某一个参数不得省略，如果省略就抛出一个错误。
+function throwIfMissing() {
+  throw new Error('Missing parameter');
+}
+function foo(mustBeProvided = throwIfMissing()) {
+  return mustBeProvided;
+}
+foo()   // Error: Missing parameter
+// 另外，可以将参数默认值设为undefined，表明这个参数是可以省略的。
+function foo(optional = undefined) { ··· }
+
+// rest参数
+// ES6引入rest参数（形式为“...变量名”），用于获取函数的多余参数，这样就不需要使用arguments对象了。
+// rest参数搭配的变量是一个数组，该变量将多余的参数放入数组中。
+function add(...values) {
+  let sum = 0;
+  for (var val of values) {
+    sum += val;
+  }
+  return sum;
+}
+add(2, 5, 3) // 10
