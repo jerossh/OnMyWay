f3fd508e1d6102c37f1d2feee6ab6c039b22d00f
Jon_shen <jerosshen@gmail.com>
2016-09-12 17:11:42 +0800

    review the good part:prototype

--- the good part/goodPart.js
+++ the good part/goodPart.js
@@ -95,3 +95,137 @@ var fade = function (node) {
     setTimeout(step, 100)
 }
 fade(document.body)
+// var for 循环
+var add_the_handlers = function (nodes) {
+  var helper = function (i) {
+    return function (e) {
+      alert(i)
+    }
+  };
+  var i;
+  for (i=0; i< nodes.length; i++) {
+    node[i].onclick = helper(i);
+  }
+}
+
+// 回调
+// 服务器接收到请求最自然写法也许是
+request = prepare_the_request();
+response = send_resquest_synchronously(request)
+display(response)
+// 网络上的同步请求导致客户端进入假死状态
+// 回调可以解决阻塞问题
+request= prepare_the_request()
+send_resquest_asynchronously(request, function(response){
+  display(response)
+})
+
+
+// module
+String.method('deentityify', function () {
+  var entity = {
+    quot: '"',
+    lt: '<',
+    gt: '>'
+  };
+  return function () {
+    return this.replace(/&([^&;]+);/g,  function(a, b) {
+      var r = entity[b];
+      return typeof r === 'string'?r:a;
+    })
+  }
+})
+// 模块模式也可以产生安全的对象
+var serial_maker = function () {
+  var prefix = ""
+  var seq = 0
+  return {
+    set_prefix: function (p) {
+      prefix = String(p)
+    },
+    set_seq: function (s) {
+      seq = s;
+    },
+    gensym: function () {
+      var result = prefix + seq;
+      seq += 1
+      return result
+    }
+  }
+}
+var seqer = serial_maker()
+seqer.set_prefix('Q')
+seqer.set_seq(1000)
+var unique = seqer.gensym()     // 'Q1000'
+
+
+// 级联，cascade
+// return this 是级联的关键
+
+
+// 柯里化
+var add1 = add.curry(1)
+document.writeIn(add1(6))
+// 扩展一个柯里化方法
+Function.method('curry', function () {
+  var slice = [].slice,
+      args = slice.apply(arguments),
+      that = this;
+  return function () {
+    return that.apply(null, args.concat(slice.apply(arguments)))
+  }
+})
+
+
+// 记忆 memorization
+// 递归函数 Fibonacci 数列
+var fabonacci = function (n) {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+for (var i = 0; i <= 10; i += 1) {
+  document.writeIn('//' + i + ': ' + fibonacci(i))
+}
+// 调用了11次，但执行了 453次， 其中442次计算刚刚计算过的值
+// 我们可以在一个 memo的数组中存储结果，存储可以隐藏在闭包中
+var fibonacci = function () {
+  var memo = [0, 1]
+  var fib = function (n) {
+    var result = memo[n]
+    if (typeof result !== 'number') {
+      result = fib(n - 1) + fib(n - 2)
+      memo[n] = result;
+    }
+    return result
+  }
+  return fib
+}();
+// 还可以做一个通用管理的
+var memoizer = function (memo, formula) {
+  var recur = function (n) {
+    var result = memo[n]
+    if (typeof result !== 'number') {
+      result = formula(recur, n);
+      memo[n] = result
+    }
+    return result
+  }
+  return recur   // recur 里面 return  result
+}
+
+
+
+
+// 继承  inheritance
+// Pseudoclassical
+// 当一个函数对象被创建时， Function 构造器产生的函数对象运行类似这样的代码：
+this.prototype = {constructor: this}
+// 新对象被赋予一个 prototype 属性，他的值一个包含 constructor 属性且属性值为该函数的对象
+// prototype 是用来存放特征的
+// constructor 属性没什么用， 重要是 prototype对象
+
+// 如果 new 是方法而不是运算符大概会这么执行
+Function.method('new', function () {
+  var that = Object.create(this.prototype)
+  var other = this.apply(that, argument);
+  return (typeof other === "object" && other) || that     // 为什么会出现不是对象的情况？
+})
