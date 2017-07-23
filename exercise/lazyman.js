75aed8569f8bb840be79cf58b0d77acf0a65933b
jerossh <jerosshen@gmail.com>
2017-07-24 01:02:54 +0800

    对箭头函数强大的绑定能力还要好好研究下...

--- exercise/lazyman.js
+++ exercise/lazyman.js
@@ -1,125 +1,125 @@
-// var log = console.log;
+var log = console.log;
 
-// function lazyMan (name) {
-//   return new _lazyMan(name);
-// }
-
-
-// // 这边应该做成调度中心
-// function _lazyMan (name) {
-//   log('hi', name);
-//   this.tasks = [];
-//   // this.tasks.push ( task );
-//   setTimeout ( () => {
-//       this.next ();
-//   }, 0 );
-// }
-
-// // 调度函数
-// _lazyMan.prototype.next = function () {
-//   var task = this.tasks.shift ();
-//   task && task ();
-// }
-
-// _lazyMan.prototype.sleep = function (time){
-//   var _this = this;
-//   this.tasks.push(function(){
-//     setTimeout(function(){
-//       console.log('睡了',time,'分钟');
-//       _this.next();
-//     }, time*1000);
-
-//   });
-//   return this;
-// }
-
-// _lazyMan.prototype.eat = function (food){
-//   var _this = this;
-//   this.tasks.push(function(){
-//     log('eat', food);
-//      _this.next();
-//   });
-//   return this;
-// }
-
-// _lazyMan.prototype.sleepFirst = function (time){
-//   var _this = this;
-//   this.tasks.unshift(function(){
-//     setTimeout(function(){
-//       console.log('先睡了',time,'分钟');
-//        _this.next();
-//     }, time*1000);
-
-//   });
-//   return this;
-// }
-
-// lazyMan('hank').eat('apple').sleep(1).eat('melo').sleepFirst(2);
-// // 总结 巧用 尾调用 和 setTimeout(fucntion(){}, 0) 的特性
+function lazyMan (name) {
+  return new _lazyMan(name);
+}
 
 
+// 这边应该做成调度中心
+function _lazyMan (name) {
+  log('hi', name);
+  this.tasks = [];
+  // this.tasks.push ( task );
+  setTimeout ( () => {
+      this.next ();
+  }, 0 );
+}
+
+// 调度函数
+_lazyMan.prototype.next = function () {
+  var task = this.tasks.shift ();
+  task && task ();
+}
+
+_lazyMan.prototype.sleep = function (time){
+  var _this = this;
+  this.tasks.push(function(){
+    setTimeout(function(){
+      console.log('睡了',time,'分钟');
+      _this.next();
+    }, time*1000);
+
+  });
+  return this;
+}
+
+_lazyMan.prototype.eat = function (food){
+  var _this = this;
+  this.tasks.push(function(){
+    log('eat', food);
+     _this.next();
+  });
+  return this;
+}
+
+_lazyMan.prototype.sleepFirst = function (time){
+  var _this = this;
+  this.tasks.unshift(function(){
+    setTimeout(function(){
+      console.log('先睡了',time,'分钟');
+       _this.next();
+    }, time*1000);
+
+  });
+  return this;
+}
+
+lazyMan('hank').eat('apple').sleep(1).eat('melo').sleepFirst(2);
+// 总结 巧用 尾调用 和 setTimeout(fucntion(){}, 0) 的特性
 
 
-// // 来个 es 6 方法
-// const log = console.log;
-// function lazyMan(name) {
-//   return new LazyMan(name);
-// }
 
-// class LazyMan {
-//     constructor ( name ) {
-//         this.tasks = [];//设置任务队列
-//         let task = (name => () => {
-//             console.log ( `Hi! This is ${name} !` );
-//             this.next ();
-//         }) ( name );
-//         this.tasks.push ( task );
-//         //通过settimeout的方法，将执行函数放入下一个事件队列中，从而达到先注册事件，后执行的目的
 
-//         setTimeout ( () => {
-//             this.next ();
-//         }, 0 );
+// 来个 es 6 方法
+const log = console.log;
+function lazyMan(name) {
+  return new LazyMan(name);
+}
 
-//     }
-//     //尾调用函数，一个任务执行完然后再调用下一个任务
-//     next () {
-//         let task = this.tasks.shift ();
-//         task && task ();
-//     }
+class LazyMan {
+    constructor ( name ) {
+        this.tasks = [];//设置任务队列
+        let task = ((name) => () => {
+            console.log ( `Hi! This is ${name} !` );
+            this.next ();
+        }) ;
+        this.tasks.push ( task );
+        //通过settimeout的方法，将执行函数放入下一个事件队列中，从而达到先注册事件，后执行的目的
 
-//     eat ( food ) {
-//         let task = (food => () => {
-//             console.log ( `Eat ${food}` );
-//             this.next ();
-//         }) ( food );
-//         this.tasks.push ( task );
-//         return this;
-//     }
+        setTimeout ( () => {
+            this.next ();
+        }, 0 );
 
-//     sleep ( time ) {
-//         let task = (time => () => {
-//             setTimeout ( () => {
-//                 console.log ( `Wake up after ${time} s!` );
-//                 this.next ();
-//             }, time * 1000 )
-//         }) ( time );
-//         this.tasks.push ( task );
-//         return this;
-//     }
+    }
+    //尾调用函数，一个任务执行完然后再调用下一个任务
+    next () {
+        let task = this.tasks.shift ();
+        task && task ();
+    }
 
-//     sleepFirst ( time ) {
-//         let task = (time => () => {
-//             setTimeout ( () => {
-//                 console.log ( `Wake up after ${time} s!` );
-//                 this.next ();
-//             }, time * 1000 )
-//         }) ( time );
-//         this.tasks.unshift ( task );//sleepFirst函数需要最先执行，所以我们需要在任务队列前面放入，然后再执行后面的任务
-//         return this;
-//     }
-// }
+    eat ( food ) {
+        let task = (food => () => {
+            console.log ( `Eat ${food}` );
+            this.next ();
+        }) ( food );
+        this.tasks.push ( task );
+        return this;
+    }
 
-// lazyMan('hank').eat('apple').sleep(1).eat('melo').sleepFirst(2);
+    sleep ( time ) {
+        let task = (time => () => {
+            setTimeout ( () => {
+                console.log ( `Wake up after ${time} s!` );
+                this.next ();
+            }, time * 1000 )
+        }) ( time );
+        this.tasks.push ( task );
+        return this;
+    }
+
+    sleepFirst ( time ) {
+        let task = (time => () => {
+            setTimeout ( () => {
+                console.log ( `Wake up after ${time} s!` );
+                this.next ();
+            }, time * 1000 )
+        }) ( time );
+        this.tasks.unshift ( task );//sleepFirst函数需要最先执行，所以我们需要在任务队列前面放入，然后再执行后面的任务
+        return this;
+    }
+}
+
+lazyMan('hank').eat('apple').sleep(1).eat('melo').sleepFirst(2);
 
 
 // 自己写一个 es 6
@@ -131,32 +131,36 @@ const log = console.log
 class LazyMan {
   constructor (name) {
     this.tasks = [];
-    this.tasks.push(name => {
-      log('你好，我是'+name);
+    this.tasks.push(() => {
+      console.log('你好，我是'+name);
       this.next();
     });
+    setTimeout(()=>{
+      this.next()
+    }, 0)
   }
   next() {
     const task = this.tasks.shift();
-    task && task();
+    task && task.call(this)
   }
   eat(food) {
-    this.tasks.push(food => {
+    this.tasks.push(() => {
       log('吃'+food+'了');
       this.next();
     })
     return this;
   }
   sleep (time) {
-    this.tasks.push(time => {
+    this.tasks.push(() => {
       log(time+'分钟后睡醒');
       this.next();
     })
     return this;
   }
   sleepFirst(time) {
-    this.tasks.unshift(tiem => {
-      log(time+'分钟后睡醒')
+    this.tasks.unshift(() => {
+      log(time+'分钟后睡醒');
+      this.next();
     })
     return this;
   }
